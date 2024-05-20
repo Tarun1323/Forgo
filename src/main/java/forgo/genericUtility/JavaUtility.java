00c9d0d37fbbf301496b1d7ca475700a444923f7
nagarjuna <nagarjuna.bogala@purpleplum.design>
2024-05-20 12:10:21 +0530

    Forgo Test Scrits

--- src/main/java/forgo/genericUtility/JavaUtility.java
+++ src/main/java/forgo/genericUtility/JavaUtility.java
@@ -1,5 +1,6 @@
 package forgo.genericUtility;
 
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -32,4 +33,31 @@ public class JavaUtility {
 		return number.toString();
 	}
 	
+	/**
+	 * This method will generate the system date and it will return the value
+	 * @return
+	 */
+	public String getSystemDate() {
+		
+		Date date = new Date();
+		String d = date.toString();
+		return d;
+	}
+	
+	/**
+	 * This method will generate the current date and time in specific format 
+	 * @return
+	 */
+	public String getSystemDateInFormat() {
+		
+		Date d = new Date();
+		 String[] dArr = d.toString().split(" ");
+		 String date = dArr[2];
+		 String month = dArr[1];
+		 String year = dArr[5];
+		 String time = dArr[3].replace(":", "-");
+		 String dateAndTime = date+" "+month+" "+year+" "+time;
+		 return dateAndTime;
+		
+	}
 }
